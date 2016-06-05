7704dcc0cf69234de675d82dcf24ced49efd95fb
DaeseongKim <scvgoe@naver.com>
2016-06-06 03:45:45 +0900

    text area validation function is added

--- static/insult.js
+++ static/insult.js
@@ -1,11 +1,35 @@
-  $(function() {
-    $('#calculate').bind('click', function() {
-      $.getJSON('http://localhost:5000/calculate', {
-        text: $('#insult-input').val()
-      }, function(data) {
+/*
+$(function() {
+	$('#calculate').bind('click', function() {
+		$.getJSON('http://localhost:5000/calculate', {
+			text: $('#insult-input').val()
+			}, function(data) {
 				console.log(data);
-        $("#result").text(data);
-      });
-      return false;
-    });
-  });
+				$("insult-input").css({'background-color' : '#FFFFEE'});
+				//        $("#result").text(data);
+			});
+		return false;
+	});
+});
+*/
+
+$(function() {
+	var textarea = $("#insult-input");
+	var resultMessage = $("#result");
+	textarea.bind('input propertychange', function() {
+		var data = Math.random() * 100;
+		data = data.toFixed(2);
+		console.log(data);
+		
+		if (data > 50) {
+			textarea.css("border-color", "#F19113");
+			resultMessage.text("Possibility of insult is " + data + "%").css("color", "#F19113");
+		}
+		else {
+			textarea.css("border-color", "#5CB85C");
+			resultMessage.text("Possibility of insult is " + data + "%").css("color", "#5CB85C");
+		}
+	});
+	return false;
+});
+
