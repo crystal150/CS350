8cfad02118579aa4fac8c3ce98b9b338c1de5223
Jaryong_Lee <crystal00@kaist.ac.kr>
2016-06-06 20:21:08 +0900

    ajax query

--- static/insult.js
+++ static/insult.js
@@ -17,19 +17,24 @@ $(function() {
 	var textarea = $("#insult-input");
 	var resultMessage = $("#result");
 	textarea.bind('input propertychange', function() {
+		/*
 		var data = Math.random() * 100;
 		data = data.toFixed(2);
 		console.log(data);
-		
-		if (data > 50) {
-			textarea.css("border-color", "#F19113");
-			resultMessage.text("Possibility of insult is " + data + "%").css("color", "#F19113");
-		}
-		else {
-			textarea.css("border-color", "#5CB85C");
-			resultMessage.text("Possibility of insult is " + data + "%").css("color", "#5CB85C");
-		}
-	});
+		*/
+		$.getJSON('http://localhost:5000/calculate', {
+			text: textarea.val()
+			}, function(data) {
+				if (data > 50) {
+				textarea.css("border-color", "#F19113");
+				resultMessage.text("Possibility of insult is " + data + "%").css("color", "#F19113");
+			}
+			else {
+				textarea.css("border-color", "#5CB85C");
+				resultMessage.text("Possibility of insult is " + data + "%").css("color", "#5CB85C");
+			}
+		});
 	return false;
+	});
 });
 
